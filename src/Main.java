518b4186ef50daae09a7f45bd036e401ffdc6d37
feeneyp <feeneyp@patricks-mbp.lan>
2016-08-07 19:02:19 +0200

    added 66 to array

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		int[ ] myArray = {11,22,33,44,55};
+		int[ ] myArray = {11,22,33,44,55,66};
 		System.out.println( myArray[ 0 ]);
 	
 		}
